8b352e9bf1942d44b455428c7844e937ce697a5b
Given Reed <greed77@gmail.com>
2015-07-09 21:39:53 -0400

    Initial commit of project files, not a lot of progress, only spent a little time on it.
    Scans subdirectories for versions, compares to a (currently hardcoded) local value... that's about it so far.

---
+++ src/frmMain.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Launcher
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheckFolder = new System.Windows.Forms.Button();
+            this.btnCheckOnline = new System.Windows.Forms.Button();
+            this.lblLocalVer = new System.Windows.Forms.Label();
+            this.lblOnlineVer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCheckFolder
+            // 
+            this.btnCheckFolder.Location = new System.Drawing.Point(33, 22);
+            this.btnCheckFolder.Name = "btnCheckFolder";
+            this.btnCheckFolder.Size = new System.Drawing.Size(119, 23);
+            this.btnCheckFolder.TabIndex = 0;
+            this.btnCheckFolder.Text = "check folder";
+            this.btnCheckFolder.UseVisualStyleBackColor = true;
+            this.btnCheckFolder.Click += new System.EventHandler(this.btnCheckFolder_Click);
+            // 
+            // btnCheckOnline
+            // 
+            this.btnCheckOnline.Location = new System.Drawing.Point(33, 92);
+            this.btnCheckOnline.Name = "btnCheckOnline";
+            this.btnCheckOnline.Size = new System.Drawing.Size(119, 23);
+            this.btnCheckOnline.TabIndex = 1;
+            this.btnCheckOnline.Text = "check online";
+            this.btnCheckOnline.UseVisualStyleBackColor = true;
+            this.btnCheckOnline.Click += new System.EventHandler(this.btnCheckOnline_Click);
+            // 
+            // lblLocalVer
+            // 
+            this.lblLocalVer.AutoSize = true;
+            this.lblLocalVer.Location = new System.Drawing.Point(243, 31);
+            this.lblLocalVer.Name = "lblLocalVer";
+            this.lblLocalVer.Size = new System.Drawing.Size(129, 13);
+            this.lblLocalVer.TabIndex = 2;
+            this.lblLocalVer.Text = "Checking for local version";
+            // 
+            // lblOnlineVer
+            // 
+            this.lblOnlineVer.AutoSize = true;
+            this.lblOnlineVer.Location = new System.Drawing.Point(246, 101);
+            this.lblOnlineVer.Name = "lblOnlineVer";
+            this.lblOnlineVer.Size = new System.Drawing.Size(120, 13);
+            this.lblOnlineVer.TabIndex = 3;
+            this.lblOnlineVer.Text = "Checking online version";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 158);
+            this.Controls.Add(this.lblOnlineVer);
+            this.Controls.Add(this.lblLocalVer);
+            this.Controls.Add(this.btnCheckOnline);
+            this.Controls.Add(this.btnCheckFolder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Launcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheckFolder;
+        private System.Windows.Forms.Button btnCheckOnline;
+        private System.Windows.Forms.Label lblLocalVer;
+        private System.Windows.Forms.Label lblOnlineVer;
+    }
+}
+
---
+++ src/frmMain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+using System.IO;
+//using System.Linq;
+//using System.Text;
+using System.Windows.Forms;
+
+namespace Launcher
+{
+    public partial class frmMain : Form
+    {
+        private String dirPath = Application.StartupPath;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheckFolder_Click(object sender, EventArgs e)
+        {
+            //TODO: check for subfolders
+            List<String> folders = new List<String>();
+            string latestVersion = "0.5";
+            foreach (string subFolderPath in Directory.GetDirectories(dirPath, "*", SearchOption.TopDirectoryOnly))
+            {
+                Console.WriteLine(subFolderPath);
+                string fullPath = Path.GetFullPath(subFolderPath).TrimEnd(Path.DirectorySeparatorChar);
+                string subFolderName = Path.GetFileName(fullPath);
+                Console.WriteLine(subFolderName);
+
+                var version1 = new Version(latestVersion);
+                var version2 = new Version(subFolderName);
+
+                var result = version1.CompareTo(version2);
+                if (result > 0)
+                    Console.WriteLine("latestVersion is greater");
+                else if (result < 0)
+                    Console.WriteLine("subFolderName is greater");
+                else
+                    Console.WriteLine("versions are equal");
+            }
+
+            //TODO: if at least one folder is available, store version of local version
+            //TODO: check online for latest version number
+            //TODO: if online is newer, download
+            //TODO: else launch local app
+        }
+
+        private void btnCheckOnline_Click(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
